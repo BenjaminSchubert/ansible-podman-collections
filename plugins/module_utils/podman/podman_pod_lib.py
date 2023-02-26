1f8152774bf9ccb87611c0467ef7d1a6da4a5d53
Benjamin Schubert <contact@benschubert.me>
2023-02-26 14:17:17 +0000

    DROPME

--- plugins/module_utils/podman/podman_pod_lib.py
+++ plugins/module_utils/podman/podman_pod_lib.py
@@ -436,6 +436,8 @@ class PodmanPodDiff:
         if after in [['bridge'], ['host'], ['slirp4netns']]:
             net_mode_after = after[0]
 
+        self.module.fail_json(before=before, after=after, msg="NO")
+
         if net_mode_after and not before:
             # Remove differences between v1 and v2
             net_mode_after = net_mode_after.replace('bridge', 'default')
