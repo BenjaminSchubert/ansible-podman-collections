957d1a0221b1d39cc8f6d6b7d96240e9982f1a56
Benjamin Schubert <contact@benschubert.me>
2023-02-26 14:30:40 +0000

    fixup! fixup! DROPME

--- plugins/module_utils/podman/podman_pod_lib.py
+++ plugins/module_utils/podman/podman_pod_lib.py
@@ -436,7 +436,7 @@ class PodmanPodDiff:
         if after in [['bridge'], ['host'], ['slirp4netns']]:
             net_mode_after = after[0]
 
-        if after and before and "slirp4netns" not in after[0] and after != ["newnet2", "net2"]:
+        if after and before and "slirp4netns" not in after[0] and after != ["newnet", "net2"]:
             self.module.fail_json(before=before, after=after, msg="NO")
 
         if net_mode_after and not before:
